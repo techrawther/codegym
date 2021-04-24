de7a2393cfb8edec682e5f949e46687d29599f78
Shahinshah <techrawther@gmail.com>
2021-04-24 22:43:14 +0530

    level5

---
+++ src/level5/task0503/Dog.java
@@ -0,0 +1,27 @@
+package level5.task0503;
+
+public class Dog {
+    String name;
+    int age;
+
+    public Dog(){
+
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public void setage(int age){
+        this.age=age;
+    }
+    public String getname(){
+        return(this.name);
+    }
+    public int getage(){
+        return(this.age);
+    }
+
+
+    public static  void main(String[] args){
+
+    }
+}
